7cb869c61676b5e4e59b6818f3013d966e26dfc5
mauricioed447 <mmacorreo1@gmail.com>
2025-09-06 17:42:12 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // <-- ESTA ES LA LÍNEA CORREGIDA
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
